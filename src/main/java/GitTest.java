821254c0f4158e48dc505ad5c94358bbc1abbf81
gyb <gaoyabinZY@163.com>
2023-12-24 18:59:46 +0800

    push test1 merge

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hot-fix test1");
         System.out.println("hot-fix test1");
         System.out.println("pull test1");
+        System.out.println("push test1");
     }
 }
