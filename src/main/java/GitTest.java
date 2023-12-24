7bdc758653ecb9222bfcd20dd7b26b199889cfb1
gyb <gaoyabinZY@163.com>
2023-12-24 16:59:02 +0800

    IDEA Second1 commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -1,5 +1,7 @@
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("sdfsdf");
+        System.out.println("sdfsdf");
+        System.out.println("sdfsdf");
     }
 }
